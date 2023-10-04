e9ef75eccb5a82075f088d150ac7c94c166d7a2d
koko <baguskokolearn@gmail.com>
2023-10-04 23:11:15 +0700

    Add : Generate ROOT CA Certificate

--- marimovpn.sh
+++ marimovpn.sh
@@ -6,24 +6,39 @@ caDir=$workdir/ca
 
 ls /etc/ssl/marimocerts/ 2> /dev/null
 if [ $(echo $?) != 0 ]; then
-#	echo -e "Step 1 = Creating Certificate Directory"
 	mkdir $workdir 2>> $log
 	if [ $(echo $?) != 0 ]; then
-		echo "Step 1 = Creating Certificate Directory | FAILED "
+		echo "Creating Certificate Directory | FAILED "
+		exit
 	else
-		echo "Step 1 = Creating Certificate Directory | SUCCESS "
+		echo "Creating Certificate Directory | SUCCESS "
 	fi
 else
-	echo "Step 1 = Creating Certificate Directory | SKIPPED | Directory Already Exist"
+	echo "Creating Certificate Directory | SKIPPED | Directory Already Exist"
 fi
 
-## Generate Root CA
-#
-#mkdir $caDir 2>> $log
-#openssl req -x509 -nodes -newkey rsa:2048 -keyout $caDir/ca.key -out $caDir/ca.crt -subj "/C=ID/ST=Jakarta/O=marimovpn/CN=ROOT CA" 2>> $log
-#
-#if [ $(echo $?) != 0 ]; then
-#	echo "Can't Generate Root CA Certificate, Please check the log!"
-#else
-#	echo "Root CA Certificate has been generated"
-#fi
+# Generate Root CA
+
+ls $workdir | grep ca > /dev/null
+
+if [ $(echo $?) != 0 ]; then
+	mkdir $caDir 2> $log
+fi
+
+ls $workdir/ca/ | grep ca.key > /dev/null && ls $workdir/ca/ | grep ca.crt > /dev/null
+
+# To do : serial belum tepat proses dibuatnya
+
+if [ $(echo $?) != 0 ]; then
+	openssl req -x509 -nodes -newkey rsa:2048 -keyout $caDir/ca.key -out $caDir/ca.crt -subj "/C=ID/ST=Jakarta/O=marimovpn/CN=ROOT CA" 2>> $log
+	if [ $(echo $?) != 0 ]; then
+		echo "Generating Root CA Certificate | FAILED "
+		exit
+	else
+		echo "Generating Root CA Certificate | SUCCESS"
+	fi
+else
+	echo 01 > $caDir/serial
+	echo "Creating Root CA Certificate | SKIPPED | Certificate  Already Exist"
+fi
+

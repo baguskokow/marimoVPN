6d1ba346c7218113a36e119af6f8b627f857989c
koko <baguskokolearn@gmail.com>
2023-10-05 00:52:08 +0700

    Upadte : serial file is correct at the script

--- marimovpn.sh
+++ marimovpn.sh
@@ -1,44 +1,59 @@
+# Colors
+white='\033[0m'
+red='\033[1;31m'
+
+# Status
+failed="${red}FAILED${white}"
+success="SUCCESS"
+skipped="SKIPPED"
+
+# working directory
 workdir=/etc/ssl/marimocerts
 log=./log/marimo.log
 caDir=$workdir/ca
 
 # Checking Certificate Directory
 
-ls /etc/ssl/marimocerts/ 2> /dev/null
+ls /etc/ssl/marimocerts/ > /dev/null
+
 if [ $(echo $?) != 0 ]; then
 	mkdir $workdir 2>> $log
 	if [ $(echo $?) != 0 ]; then
-		echo "Creating Certificate Directory | FAILED "
+		echo -e "Creating Certificate Directory			| $failed "
 		exit
 	else
-		echo "Creating Certificate Directory | SUCCESS "
+		echo -e "Creating Certificate Directory			| $success "
 	fi
 else
-	echo "Creating Certificate Directory | SKIPPED | Directory Already Exist"
+	echo -e "Creating Certificate Directory			| $skipped | Directory Already Exist"
 fi
 
-# Generate Root CA
+### Generate Root CA
 
+# Checking "ca" directory
 ls $workdir | grep ca > /dev/null
 
 if [ $(echo $?) != 0 ]; then
 	mkdir $caDir 2> $log
+	if [ $(echo $?) != 0 ]; then
+		echo "Exited"
+		exit
+	fi
 fi
 
+# Checking certificate
 ls $workdir/ca/ | grep ca.key > /dev/null && ls $workdir/ca/ | grep ca.crt > /dev/null
 
-# To do : serial belum tepat proses dibuatnya
-
+# Generate Certificate
 if [ $(echo $?) != 0 ]; then
-	openssl req -x509 -nodes -newkey rsa:2048 -keyout $caDir/ca.key -out $caDir/ca.crt -subj "/C=ID/ST=Jakarta/O=marimovpn/CN=ROOT CA" 2>> $log
+		openssl req -x509 -nodes -newkey rsa:2048 -keyout $caDir/ca.key -out $caDir/ca.crt -subj "/C=ID/ST=Jakarta/O=marimovpn/CN=ROOT CA" 2>> $log && echo "01" | tee $caDir/serial > /dev/null
 	if [ $(echo $?) != 0 ]; then
-		echo "Generating Root CA Certificate | FAILED "
+		echo -e "Generating Root CA Certificate			| $failed"
 		exit
 	else
-		echo "Generating Root CA Certificate | SUCCESS"
+		echo -e "Generating Root CA Certificate			| $success"
 	fi
 else
-	echo 01 > $caDir/serial
-	echo "Creating Root CA Certificate | SKIPPED | Certificate  Already Exist"
+	echo -e "Creating Root CA Certificate			| $skipped | Certificate  Already Exist"
 fi
 
